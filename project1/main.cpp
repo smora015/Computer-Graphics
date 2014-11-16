4f3c9eb7868eded95795a265d7fc6565185d961f
Sergio Morales <smora015@ucr.edu>
2014-11-16 12:34:29 -0800

    Rendering works without zbuffer!

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,6 +1,6 @@
 // Name: Sergio Morales
 // Quarter, Year: Fall 2014
-// Lab: 3
+// Project: 1
 //
 // This file is to be modified by the student.
 // main.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 #include <math.h>
 #include "point2d.h"
 #include "point3d.h"
@@ -18,7 +19,6 @@ using namespace std;
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
-
 
 /// ------------------------- Struct/Type Definitions ---------------------///
 typedef struct clr // Holds different triangle colors
@@ -47,11 +47,12 @@ typedef struct Triangle
 } Triangle;
 
 /// --------------------------- Global Variables -------------------------///
-clr colors[5] = { clr(.5, .1, .1), clr(.1, .5, .1), clr(.1, .1, .5), clr(.5, .75, 1), clr(.3, .2, .6) };
-vector< Point2D > points; // Container for 2D points
-vector< Point3D > cartesian_points;
-vector< Triangle > triangles;
-
+const int DEPTH_INIT = -999; // This is our initial z-value
+clr colors[5] = { clr(.5, .1, .1), clr(.1, .5, .1), clr(.1, .1, .5), clr(.5, .75, 1), clr(.3, .2, .6) }; // Array of 5 colors to cycle through
+vector< Point2D > points; // Container for 2D points. Used for scan conversion
+vector< Point3D > cartesian_points; // Container of 3D points. Raw point data
+vector< Triangle > triangles; // Container of 3 sets of 3D points, or a triangle. Used to get points array data
+vector< vector< double > > zbuffer( WINDOW_WIDTH, vector<double>( WINDOW_HEIGHT, DEPTH_INIT ) ); // Initialize our zbuffer to 800x800 points at DEPTH_INIT
 
 
 /// -------------------------- Function Declarations --------------------///
@@ -67,11 +68,23 @@ void GL_mouse(int button, int state, int x, int y);
 // Keyboard event handler
 void GL_key( unsigned char key, int x, int y );
 
+// Keyboard event handler
+void GL_spec_key( int key, int x, int y );
+
 //Initializes OpenGL attributes
 void GLInit(int* argc, char** argv);
 
 // Returns appropriate window coordinates
 Point2D GLScreenToWindowCoordinates( const Point2D & p );
+
+// Gets the average of all vertices in our points from model
+Point3D average_vertices();
+
+// Converts degrees to radians
+double get_rad( double deg );
+
+// Rotates the screen by the specified angle	
+void rotate( double q );
 
 // Determines max y value out of 3 points.
 // @param: number   The starting point in our points array for the 3 vertices.
@@ -125,6 +138,7 @@ void parse_model( string file )
     int x,y,z;
     for( int i = 0; i < num_points; ++i )
     {
+				// Populate the cartesian points container
         model >> x;
         model >> y;
         model >> z;
@@ -133,6 +147,7 @@ void parse_model( string file )
     
     for( int i = 0; i < num_triangles; ++i )
     {
+				// Populate the triangles container with cartesian_points indices
         model >> x;
         model >> y;
         model >> z;
@@ -155,11 +170,11 @@ void GLInit(int* argc, char** argv)
   // Register rendering function
   glutDisplayFunc( GL_render );
 	
-  // NEW: Register mouse event function
-  //glutMouseFunc( GL_mouse );
+  // Register special keyboard event function
+  glutSpecialFunc( GL_spec_key );
 
   // NEW: Register keyboard event function
-  //glutKeyboardFunc( GL_key );
+  glutKeyboardFunc( GL_key );
 
   // The default view coordinates is (-1.0, -1.0) bottom left & (1.0, 1.0) top right.
   // For the purposes of this lab, this is set to the number of pixels
@@ -172,60 +187,90 @@ void GL_render()
 {
   glClear( GL_COLOR_BUFFER_BIT );
 
-  // Orthogonal projection
+  // Traverse through points in all polygons
   for( int i = 0; i < triangles.size(); ++i )
   {
+			// Grab the 2D points and push them to points container
       Point3D temp;
     
       temp = cartesian_points[ triangles[i].p1 ];
       points.push_back( Point2D( temp.x, temp.y ) );
+      cout << "p1: " << temp.x << " " << temp.y << " " << temp.z << endl;
       
-      temp = cartesian_points[ triangles[i].p2 ];
+
+			temp = cartesian_points[ triangles[i].p2 ];
       points.push_back( Point2D( temp.x, temp.y ) );
-      
+			cout << "p2: " << temp.x << " " << temp.y << " " << temp.z << endl;
+
+
       temp = cartesian_points[ triangles[i].p3 ];
       points.push_back( Point2D( temp.x, temp.y ) );
-      
+      cout << "p3: " << temp.x << " " << temp.y << " " << temp.z << endl << endl;
   }
-  // Render Bezier curve after 2 mouse clicks on screen
-  if( ( (points.size() % 3) == 0) && points.size() > 0)
-  {
-    for( int i = 0; i < points.size()/3; ++i)
-      renderTPolygon(i); // Render triangle polygon
+
+	// Go through points container and output pixels, based off zbuffer as well
+  for( int i = 0; i < points.size()/3; ++i)
+  	renderTPolygon(i); // Render triangle polygon
       
-  }
+	cout << "Triangles size: " << triangles.size() <<  " points size: " << points.size() << endl;
+	points.clear();
   glutSwapBuffers();
 }
 
 
-
-void GL_mouse(int button, int state, int x, int y)
+void GL_spec_key( int key, int x, int y )
 {
-  // Left click gets points
-  if( button == GLUT_LEFT_BUTTON && state == GLUT_UP )
-  {
-    // Get first/second coordinate
-    Point2D temp = GLScreenToWindowCoordinates( Point2D(x, y) );
-    
-
-    points.push_back( temp );
-
-    
-    // After second click, form Bezier curve
-    if( points.size() % 3 == 0 && points.size() > 0)
-      glutPostRedisplay();
-  }
-  
-  return;
-} 
+  if(key == GLUT_KEY_RIGHT) // Rotate along y-axis -->
+	{
+		cout << "Rotated left!" << endl;
+		rotate( -25 );
+    points.clear();
+		glutPostRedisplay();
+	}
+	else if(key == GLUT_KEY_LEFT) // Rotate along y-axis <--
+	{
+		cout << "Rotated right!" << endl;
+		rotate( 25 );
+		points.clear();
+		glutPostRedisplay();
+	}
+}
 
 void GL_key( unsigned char key, int x, int y )
 {
   if( key == '0' )
   {
-    points.resize(0);
+		//cartesian_points.clear();
+    points.clear();
+		//triangles.clear();
+		zbuffer.clear();
+		zbuffer = vector< vector<double> >( WINDOW_WIDTH, vector<double>( WINDOW_HEIGHT, DEPTH_INIT ) );
+
     glutPostRedisplay();
   }
+	else if( key == '+' )
+  {
+		//cartesian_points.clear();
+    points.clear();
+		//triangles.clear();
+		//zbuffer.clear();
+		//zbuffer = vector< vector<double> >( WINDOW_WIDTH, vector<double>( WINDOW_HEIGHT, DEPTH_INIT ) );
+
+		cout << "Zoomed in!" << endl;
+    glutPostRedisplay();
+  }
+	else if( key == '-' )
+  {
+		//cartesian_points.clear();
+    points.clear();
+		//triangles.clear();
+		//zbuffer.clear();
+		zbuffer = vector< vector<double> >( WINDOW_WIDTH, vector<double>( WINDOW_HEIGHT, DEPTH_INIT ) );
+
+    glutPostRedisplay();
+
+		cout << "Zoomed out!" << endl;
+	}
 
   return;
 }
@@ -252,6 +297,75 @@ Point2D GLScreenToWindowCoordinates( const Point2D & p )
 }
 
 
+Point3D average_vertices()
+{
+	Point3D average;
+	for( int i = 0; i < cartesian_points.size(); ++i)
+		average += cartesian_points[i];
+
+	float div = cartesian_points.size();
+	average /= div;
+	return average;
+
+}
+
+double degrees_to_radians( double deg )
+{
+	return deg*3.14/180;
+}
+	
+void rotate( double q )
+{
+	Point3D avg = average_vertices(); // Get average of all vertices to translate polygon to origin
+
+	for( int i = 0; i < cartesian_points.size(); ++i ) // Displace vertices to axis
+		cartesian_points[i] -= avg;
+
+	for( int i = 0; i < cartesian_points.size(); ++i ) // Rotate
+	{
+		Point3D rotate;
+		rotate.x = cartesian_points[i].x*cos( degrees_to_radians(q)) + cartesian_points[i].z*sin( degrees_to_radians(q));
+		rotate.y = cartesian_points[i].y;
+		rotate.z = -cartesian_points[i].x*sin( degrees_to_radians(q)) + cartesian_points[i].z*cos( degrees_to_radians(q));
+		cartesian_points[i] = rotate;
+	}
+
+	for( int i = 0; i < cartesian_points.size(); ++i ) // Translate back to original position 
+		cartesian_points[i] += avg;
+	
+		
+}
+
+
+bool update_zbuffer( double x, double y, int number )
+{
+	//first calculate plane of current triangle
+	//do this by finding a normal vector, or the cross product of two vectors
+
+	Point3D p1v = cartesian_points.at( triangles.at(number).p1 );//stores the values of each vertex in the triangle
+	Point3D p2v = cartesian_points.at( triangles.at(number).p2 );
+	Point3D p3v = cartesian_points.at( triangles.at(number).p3 );
+	
+	Point3D vector1 = p1v-p2v; //finds two vectors in the plane equal to the triangle
+	Point3D vector2 = p1v-p3v;
+	
+	Point3D norm = vector1.cross( vector2 ); // the normal vector to the plane
+	double D = norm.x*p1v.x + norm.y*p1v.y + norm.z*p1v.z;	
+	double z = ( D -norm.x*x - norm.y*y )/norm.z; //will find the z val of the passed in x&y
+	
+	if( z > zbuffer.at( (x >= 800) ? 799 : x ).at( (y >= 800) ? 799 : y ) ||
+			zbuffer.at( (x >= 800) ? 799 : x ).at( (y >= 800) ? 799 : y ) == DEPTH_INIT  )//point is only or closest point
+	{
+		//cout << "x: " << x << " y: " << y << " z: " << z << endl;
+		zbuffer.at( (x >= 800) ? 799 : x ).at( (y >= 800) ? 799 : y ) = z; // we update the zbuff
+		return true;// we want to render this point
+	}
+
+
+	return false; //there is already a closer point at this value
+}
+
+
 // ------ Triangle functions ------ //
 int ymax( int number ) // Determines max y value out of 3 points
 {
@@ -260,7 +374,7 @@ int ymax( int number ) // Determines max y value out of 3 points
     for(int i = number*3; i < number*3 + 3; ++i)
     {
       if( points[i].y >= points[max].y )
-	max = i;
+				max = i;
     }
     
     return max;
@@ -315,7 +429,7 @@ void renderTPolygon( int number )
     Point2D mid;
     if( find_ymid( mid, y_min, y_max, number ) ) // Case 1: Split triangle into 2 
     {
-      // Fetch Pnew. Confirmed to work.
+      // Fetch Pnew.
       double s = (y_max.x - y_min.x) / ( y_max.y - y_min.y );
       double c = (y_min.x - s*y_min.y );
       Point2D p_new( s*mid.y + c, mid.y );
@@ -340,7 +454,8 @@ void renderTPolygon( int number )
         // Render pixels, starting from left slope of the triangle, to the right slope.
         for ( int x = (int) x0; x < x1; ++x )
         {
-            renderPixel(x, y, colors[number % 5].R, colors[number % 5].G, colors[number % 5].B);
+						if( update_zbuffer(x, y, number) )
+  	          renderPixel(x, y, colors[number % 5].R, colors[number % 5].G, colors[number % 5].B);
         } 
       }
       
@@ -364,14 +479,15 @@ void renderTPolygon( int number )
         // Render pixels
         for ( int x = (int) x0; x < x1; ++x )
         { 
-          renderPixel(x, y, colors[number % 5].R, colors[number % 5].G, colors[number % 5].B);
+					if( update_zbuffer(x, y, number) )
+     				renderPixel(x, y, colors[number % 5].R, colors[number % 5].G, colors[number % 5].B);
         }
       }
 
     }
     else // Case 2 and 3: Triangle has 2 ymins or 2 ymaxs
     {
-      // Grab non y-min/y-max
+      // Grab second y-min/y-max
       int p_newn = number*3;
       for(int i = number*3; i < number*3 + 3; ++i)
       {
@@ -413,8 +529,9 @@ void renderTPolygon( int number )
 
           // Render pixels
           for ( int x = (int) x0; x < x1; ++x )
-          { 
-            renderPixel(x, y, colors[number % 5].R, colors[number % 5].G, colors[number % 5].B);
+          {
+						if( update_zbuffer(x, y, number) )
+ 	           renderPixel(x, y, colors[number % 5].R, colors[number % 5].G, colors[number % 5].B);
           }
         }
       }
@@ -449,7 +566,8 @@ void renderTPolygon( int number )
             // Render pixels, starting from left slope of the triangle, to the right slope.
             for ( int x = (int) x0; x < x1; ++x )
             {
-              renderPixel(x, y, colors[number % 5].R, colors[number % 5].G, colors[number % 5].B);
+							if( update_zbuffer(x, y, number) )
+  	            renderPixel(x, y, colors[number % 5].R, colors[number % 5].G, colors[number % 5].B);
             } 
               
           }
